cd2eee720b411773afe890230bc80f8e90f419c3
ReynaldArro <sleight.nald@gmail.com>
2023-05-07 03:39:06 +0800

    modified endpoints

--- backend/server.js
+++ backend/server.js
@@ -138,6 +138,7 @@ app.get("/exercises", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log(`Server started at port: 5000`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started at port: ${port}`);
 });
